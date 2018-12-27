aaacc7ae1bfd5f84588a7936e92454368f41af30
Rakesh <rakeshkumar8900@gmail.com>
2018-12-27 13:18:56 +0530

    new production build after api url set to secure

---
+++ docs/precache-manifest.657f7003597055ee6da57278b333ac9a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a4c608386a9b2281078",
+    "url": "/nobroker_property_app/static/js/runtime~main.4a4c6083.js"
+  },
+  {
+    "revision": "68c5a71b704d093f7587",
+    "url": "/nobroker_property_app/static/js/main.68c5a71b.chunk.js"
+  },
+  {
+    "revision": "b8fde54ede2f91779ea1",
+    "url": "/nobroker_property_app/static/js/1.b8fde54e.chunk.js"
+  },
+  {
+    "revision": "a2dbc1b6102ad9496bbb463587104d45",
+    "url": "/nobroker_property_app/index.html"
+  }
+];
