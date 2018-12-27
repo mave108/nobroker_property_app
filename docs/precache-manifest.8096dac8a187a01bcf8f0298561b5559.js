dbd623265bc16bf6123188835367107855c62f0d
Rakesh <rakeshkumar8900@gmail.com>
2018-12-27 13:04:39 +0530

    new production build

---
+++ docs/precache-manifest.8096dac8a187a01bcf8f0298561b5559.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a4c608386a9b2281078",
+    "url": "/nobroker_property_app/static/js/runtime~main.4a4c6083.js"
+  },
+  {
+    "revision": "59ea2a7c9f9d865a5bcd",
+    "url": "/nobroker_property_app/static/js/main.59ea2a7c.chunk.js"
+  },
+  {
+    "revision": "b8fde54ede2f91779ea1",
+    "url": "/nobroker_property_app/static/js/1.b8fde54e.chunk.js"
+  },
+  {
+    "revision": "cbc98086d0718731eb3c747e8126ad6c",
+    "url": "/nobroker_property_app/index.html"
+  }
+];
