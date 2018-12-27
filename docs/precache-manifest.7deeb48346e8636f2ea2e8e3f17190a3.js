fe12132a03dd48f2bde9c57e511adeb0574fe2c5
Rakesh <rakeshkumar8900@gmail.com>
2018-12-27 12:59:12 +0530

    Initilized docs folder

---
+++ docs/precache-manifest.7deeb48346e8636f2ea2e8e3f17190a3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "59ea2a7c9f9d865a5bcd",
+    "url": "/static/js/main.59ea2a7c.chunk.js"
+  },
+  {
+    "revision": "b8fde54ede2f91779ea1",
+    "url": "/static/js/1.b8fde54e.chunk.js"
+  },
+  {
+    "revision": "b6236b7597fd811ca27bf059d9916349",
+    "url": "/index.html"
+  }
+];
